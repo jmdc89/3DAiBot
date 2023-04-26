f4993710f8c6cae32bfc3974792ee9d90a23c774
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-26 09:13:39 +0300

    TextToSpeech
    Init

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { TextToSpeech } from '@/components/TextToSpeech'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className='h-screen'>
-      {/* textToSpeech */}
+      <TextToSpeech />
       {/* ChatBotCanvas */}
     </main>
   )
