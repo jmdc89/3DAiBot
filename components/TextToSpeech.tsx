06c25b77f58f4c4634a44a4092405ce7840e597a
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-26 09:30:29 +0300

    Update TextToSpeech.tsx
    button styles

--- components/TextToSpeech.tsx
+++ components/TextToSpeech.tsx
@@ -9,7 +9,7 @@ export const TextToSpeech = () => {
                 type="text"
                 placeholder="What do you want to know..."
             />
-            <button>Ask</button>
+            <button className='text-[#b00c3f] p-2 border border-[#b00c3f] rounded-lg disabled:text-blue-100 disabled:cursor-not-allowed disabled:bg-gray-500 hover:scale-110 hover:text-black hover:bg-[#b00c3f] duration-300 transition-all'>Ask</button>
         </form>
     </div>
   )
