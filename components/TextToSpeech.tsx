f84ea048b95370879a85ef63f9eae1f9f796b1b5
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-26 14:53:14 +0300

    Update TextToSpeech.tsx
    responsive input styles

--- components/TextToSpeech.tsx
+++ components/TextToSpeech.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const TextToSpeech = () => {
   return (
-    <div>
-        <form>
+    <div className='flex justify-center top-0 z-50'>
+        <form className='flex fixed mx-auto bottom-2 space-x-2 pt-2'>
             <input
-                className='bg-transparent w-[510px] border border-[#b00c3f]/80 outline-none rounded-lg placeholder:text-[#b00c3f] p-2 text-[#b00c3f]'
+                className='bg-transparent w-[300px] sm:w-[500px] xs:w-[250px] border border-[#b00c3f]/80 outline-none rounded-lg placeholder:text-[#b00c3f] p-2 text-[#b00c3f]'
                 type="text"
                 placeholder="What do you want to know..."
             />
