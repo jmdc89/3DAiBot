2c820e64995ffa8568d1bf7f52f32b7d20f01e17
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-26 09:23:17 +0300

    Update TextToSpeech.tsx
    placeholder

--- components/TextToSpeech.tsx
+++ components/TextToSpeech.tsx
@@ -5,8 +5,10 @@ export const TextToSpeech = () => {
     <div>
         <form>
             <input
-            className='bg-transparent w-[510px] border border-[#b00c3f]/80'
-            type="text" />
+                className='bg-transparent w-[510px] border border-[#b00c3f]/80 outline-none rounded-lg placeholder:text-[#b00c3f] p-2 text-[#b00c3f]'
+                type="text"
+                placeholder="What do you want to know..."
+            />
             <button>Ask</button>
         </form>
     </div>
