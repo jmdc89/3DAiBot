7005185fad3d383fa3c855d6a3ead38850e9469a
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-30 14:49:25 +0300

    open api
    open api

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,12 +1,30 @@
 import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 export async function GET(request: Request) {
-    return new Response('Hello, from openai!')
-  }
-  
-  export async function POST(request: Request) {
-    const {userText} = await request.json();
-    console.log(userText)
-    return NextResponse.json({message: userText}, {status:200});
-  }
-  
+	return new Response("Hello, Next.js!");
+}
+
+export async function POST(request: Request) {
+	const { userText } = await request.json();
+
+	// const completion = await openai.createChatCompletion({
+	// 	model: "gpt-3.5-turbo",
+	// 	messages: [{ role: "user", content: userText }],
+	// });
+
+	// const aiMessage = completion.data.choices[0].message?.content;
+
+	return NextResponse.json(
+		{
+			message:
+				"I am not allowed to answer that, human",
+		},
+		{ status: 200 }
+	);
+}
