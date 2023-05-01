038b2552960cdd8fb9c0aa7e3de919c8172f4eb0
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-05-01 16:16:47 +0300

    Update ChatBotCanvas.tsx
    Orbitcontrols

--- components/ChatBotCanvas.tsx
+++ components/ChatBotCanvas.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 
 const ChatBotCanvas = () => {
     return (
     <Canvas>
+        <OrbitControls />
         <mesh>
             <boxGeometry />
         </mesh>
