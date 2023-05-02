87bb017f24821ae2ddc8db8885f99b4225780095
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-05-02 14:31:50 +0300

    Update ChatBotCanvas.tsx
    useEffect

--- components/ChatBotCanvas.tsx
+++ components/ChatBotCanvas.tsx
@@ -2,18 +2,22 @@
 
 import { OrbitControls, useAnimations, useGLTF} from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Head = () => {
     const model = useGLTF("head.glb");
     const animations = useAnimations(model.animations, model.scene);
 	const action = animations.actions.Animation;
+    useEffect(() => {
+        // action?.play();
+    },[action])
+
     return < primitive object={model.scene} scale={3} />
 }
 
 const ChatBotCanvas = () => {
     return (
-    <Canvas>
+    <Canvas className='z-0'>
         <OrbitControls
 				enableZoom={false}
 				enableDamping
